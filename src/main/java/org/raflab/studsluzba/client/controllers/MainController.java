53e9f7a3a35df3ce134aadf581c9ee747a55d14e
Shegy <149938356+sShegy@users.noreply.github.com>
2026-01-17 21:23:00 +0100

    Front i Back dugme dodati

--- src/main/java/org/raflab/studsluzba/client/controllers/MainController.java
+++ src/main/java/org/raflab/studsluzba/client/controllers/MainController.java
@@ -1,6 +1,7 @@
 package org.raflab.studsluzba.client.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import org.raflab.studsluzba.client.core.NavigationManager;
 import org.springframework.stereotype.Controller;
 
@@ -9,8 +10,32 @@ public class MainController {
 
     private final NavigationManager navigationManager;
 
+    @FXML private Button btnBack;
+    @FXML private Button btnForward;
+
     public MainController(NavigationManager navigationManager) {
         this.navigationManager = navigationManager;
+    }
+
+    @FXML
+    public void initialize() {
+
+        if (btnBack != null) {
+            btnBack.disableProperty().bind(navigationManager.canGoBackProperty().not());
+        }
+        if (btnForward != null) {
+            btnForward.disableProperty().bind(navigationManager.canGoForwardProperty().not());
+        }
+    }
+
+    @FXML
+    public void handleBack() {
+        navigationManager.goBack();
+    }
+
+    @FXML
+    public void handleForward() {
+        navigationManager.goForward();
     }
 
     @FXML
